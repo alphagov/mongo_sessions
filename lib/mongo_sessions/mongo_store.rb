4b309101e03a50cecddef1b84ac10d4182d2d833
Kimmo Lehto <kimmo.lehto@maventa.com>
2012-01-17 13:56:13 +0200

    enabled logging, use :logger => Proc.new {|l| logger.debug l}

--- lib/mongo_sessions/mongo_store.rb
+++ lib/mongo_sessions/mongo_store.rb
@@ -6,6 +6,10 @@ module MongoSessions
     def collection
       @collection
     end
+
+    def logger(msg)
+      @logger.call("[mongo_sessions] #{msg}") unless @logger.nil?
+    end
     
     def initialize(app, options = {})
       require 'mongo'
@@ -17,7 +21,8 @@ module MongoSessions
       end
       
       @collection = options[:collection].respond_to?(:call) ? options[:collection].call : options[:collection]
-      
+      @logger = options[:logger].respond_to?(:call) ? options[:logger] : nil
+      logger "initialized with #{options.inspect}"
       super
     end
     
@@ -29,15 +34,19 @@ module MongoSessions
 
     private
     def get_session(env, sid = nil)
+      logger "get_session called with #{sid.inspect}"
       sid ||= generate_sid
       data = collection.find_one('_id' => sid)
+      logger "get_session data: #{data.inspect.to_s}"
       unless data.nil?
         collection.update({'_id' => sid}, {'l' => Time.now})
+        logger "updated last for #{sid}"
       end
       [sid, data ? unpack(data['s']) : {}]
     end
 
     def set_session(env, sid, session_data, options = {})
+      logger "set_session called with sid:#{sid.inspect} data:#{session_data.inspect}"
       sid = sid.join('&') if sid.kind_of?(Array)
       sid ||= generate_sid
       collection.update(
@@ -54,6 +63,7 @@ module MongoSessions
     end
 
     def destroy_session(env, sid, options = {})
+      logger "destroy_session called with sid:#{sid.inspect} options:#{options.inspect}"
       collection.remove({'_id' => sid})
       options[:drop] ? nil : set_session(env, nil, {})
     end
